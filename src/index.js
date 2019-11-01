e2e01185ea8ea96dfc2e37a9489ee5ebcf2af7eb
dziok <dawidtopa1@gmail.com>
2019-11-02 00:51:31 +0100

    World navigation : moving, range

--- src/index.js
+++ src/index.js
@@ -1,37 +1,3 @@
-import * as THREE from 'three'
-
-import {
-    scene,
-    animateStart,
-    enableMouseEventsOnScene,
-    disableMouseEventsOnScene,
-    animateStop
-} from './threeConfig'
+import './worldNavigation'
 
 
-
-var geometry = new THREE.BoxGeometry(10, 10, 10)
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-var cube = new THREE.Mesh(geometry, material)
-cube.position.set(0, 0, 0)
-cube.userData.clickable = true
-scene.add(cube)
-
-var geometry = new THREE.BoxGeometry(100, 1, 100);
-var material = new THREE.MeshBasicMaterial({ color: 0xffff22 });
-var cube2 = new THREE.Mesh(geometry, material)
-cube2.userData.clickable = false
-cube2.position.set(0, 0, 0)
-scene.add(cube2);
-
-const testFunc = (element) => {
-    console.log('clicked elemenmt', element)
-}
-enableMouseEventsOnScene(undefined, testFunc)
-
-setTimeout(() => {
-    console.log('minelo')
-    disableMouseEventsOnScene()
-}, 2000)
-
-animateStart()
---
+++ src/worldNavigation/index.js
@@ -0,0 +1,101 @@
+import * as THREE from 'three'
+
+import {
+    scene,
+    animateStart,
+    camera,
+    enableMouseEventsOnScene,
+    disableMouseEventsOnScene,
+    animateStop
+} from '../threeConfig'
+
+const speed = 3
+let distance = 0
+let range = 100
+const playerY = 20
+let clickedPosition = new THREE.Vector3(0, playerY, 0)
+let directionVect = new THREE.Vector3(0, 0, 0)
+
+let showCircle = true
+
+var boardGeometry = new THREE.PlaneGeometry(500, 500);
+var boardMaterial = new THREE.MeshBasicMaterial({ color: 0xc0fa36, side: THREE.DoubleSide });
+var board = new THREE.Mesh(boardGeometry, boardMaterial);
+boardGeometry.rotateX(-Math.PI / 2)
+scene.add(board);
+
+var cubeContainer = new THREE.Object3D()
+
+
+var cubeGeometry = new THREE.BoxGeometry(10, 10, 10)
+var cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xfab0cd })
+var cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+cubeContainer.position.set(0, playerY, 0)
+cube.userData.clickable = true
+cubeContainer.add(cube)
+scene.add(cubeContainer)
+
+var circleGeometry = new THREE.CircleGeometry(range, 320);
+circleGeometry.rotateX(-Math.PI / 2)
+var circleMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+var circle = new THREE.Mesh(circleGeometry, circleMaterial);
+circle.position.set(cubeContainer.position.x, 0.1, cubeContainer.position.z)
+circle.userData.moveHere = true
+scene.add(circle)
+
+const moveCube = ({ element, clickPosition }) => {
+
+    if (element.userData.moveHere) {
+
+        cube.rotation.y = Math.atan2(
+            cubeContainer.position.clone().x - clickedPosition.x,
+            cubeContainer.position.clone().z - clickedPosition.z
+        );
+
+        clickedPosition = clickPosition
+        clickedPosition.setY(playerY)
+        directionVect = clickedPosition.clone().sub(cubeContainer.position.clone()).normalize()
+    }
+
+}
+
+enableMouseEventsOnScene(undefined, moveCube)
+
+
+const update = () => {
+    circle.position.set(cubeContainer.position.x, 0.1, cubeContainer.position.z)
+    let cubePositionOnBoard = cubeContainer.position.clone()
+    distance = cubePositionOnBoard.distanceTo(clickedPosition)
+
+    if (distance > speed) {
+        if (showCircle) {
+            range -= distance
+            var newCircleGeometry = new THREE.CircleGeometry(range, 320);
+            newCircleGeometry.rotateX(-Math.PI / 2)
+            circleGeometry.dispose()
+            circle.geometry = newCircleGeometry
+            showCircle = !showCircle
+            scene.remove(circle)
+            disableMouseEventsOnScene()
+        }
+        cubeContainer.translateOnAxis(directionVect, speed)
+
+        //camera.position.x = cubeContainer.position.x
+        // camera.position.z = cubeContainer.position.z + 400
+        //  camera.position.y = cubeContainer.position.y + 400
+        //camera.lookAt(cubeContainer.position)
+    } else {
+        if (!showCircle) {
+            showCircle = !showCircle
+            if (range > speed + cube.geometry.parameters.width) scene.add(circle)
+            else {
+                console.log('koniec paliwa')
+            }
+
+            enableMouseEventsOnScene(undefined, moveCube)
+        }
+    }
+
+
+}
+animateStart(update)
