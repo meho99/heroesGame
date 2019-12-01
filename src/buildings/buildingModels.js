595acf67c31fdd2fc7c5446bf06a1195bb32dd03
dziok <dawidtopa1@gmail.com>
2019-12-01 16:55:19 +0100

    diffrent buildings logic init

--- src/buildings/buildingModels.js
+++ src/buildings/buildingModels.js
@@ -5,7 +5,7 @@ export const models = [
     {
         id: 0,
         name: 'testBuilding',
-        model: addModel("building", { x: 0, z: 0 }),
+        model: () => addModel("building", { x: 0, z: 0 }),
         positionCorrections: { x: -3, y: -3, z: 7.5 }
     }
 ]
